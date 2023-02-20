e18c72f504b73ad2bf638382dd46e495fa784211
Inevdhoven <inevdhoven@gmail.com>
2023-02-20 11:26:25 +0100

    Added js for hero and add hash router with switch

--- spa/js/script.js
+++ spa/js/script.js
@@ -1,14 +1,34 @@
 // Variables
 const keyAPI = 'A5AmwJwG'
+const type = 'pot'
 const urlAPI = `https://www.rijksmuseum.nl/api/nl/collection?key=${keyAPI}&ps=100`
+const urlAPIType = `https://www.rijksmuseum.nl/api/nl/collection?key=${keyAPI}&ps=100` + `&type=${type}`
 
 // Functions
 async function fetchData() {
     const data = await fetch(urlAPI) // Data wordt opgehaald uit de API
     .then(response => response.json()) // Data wordt omgezet naar JSON
     .then(data => { 
+        hero(data) // data wordt doorgegeven aan de functie hero
         top10(data) // data wordt doorgegeven aan de functie top10
     })
+    const data2 = await fetch(urlAPIType) // Data wordt opgehaald uit de API
+    .then(response => response.json()) // Data wordt omgezet naar JSON
+    .then(data2 => {
+        typeJars(data2)
+    })
+}
+
+function hero(data) {
+    const heroObject = data.artObjects.find(artObject => artObject.objectNumber === 'SK-A-3064') // De afbeelding van het object met het objectnummer SK-A-5 wordt opgehaald
+    const heroImage = heroObject.webImage.url // De url van de afbeelding wordt opgehaald
+    const hero = document.querySelector('main section:first-of-type section:first-of-type h1') // De hero wordt geselecteerd
+    console.log(heroImage);
+    hero.insertAdjacentHTML(
+        'afterend', 
+        `<img src="${heroImage}" alt="${heroObject.title}">`
+        ) // De afbeelding wordt in de hero gezet
+    console.log(heroObject.title);
 }
 
 function top10(data) { // De functie top10 wordt aangemaakt en krijgt de data mee
@@ -28,6 +48,35 @@ function top10(data) { // De functie top10 wordt aangemaakt en krijgt de data me
         )
     })
 }
+
+function typeJars(data) {
+    const jars = data.artObjects.map(artObject => artObject)
+    console.log(jars);
+}
+
+function onRouteChanged() {
+    const hash = window.location.hash;
+    const routerView = document.querySelector('main');
+    console.log(window.location.hash);
+
+    if(!(routerView instanceof HTMLElement)) {
+        throw new ReferenceError('No router view element available for rendering')
+    }
+
+    switch(hash) {
+        case '#home':
+            routerView.innerHTML = '<h1>Home page</h1>';
+            break;
+        case '#about':
+            routerView.innerHTML = '<h1>About page</h1>';
+            break;
+        default:
+            routerView.innerHTML = '<h1>404 - Page Not Found</h1>';
+            break;
+    }
+}
+
+window.addEventListener('hashchange', onRouteChanged);
 
 // Logica
 fetchData();
