89d739837cdd2d0d73afaeffc3b0f38be757f408
Inevdhoven <inevdhoven@gmail.com>
2023-02-24 16:17:03 +0100

    Added involvedMaker and objectNumber for fetch

--- spa/js/modules/data.js
+++ spa/js/modules/data.js
@@ -2,10 +2,9 @@ import { displayLoader, hideLoader } from "./loader.js"
 import { hero, top10 } from "./home.js"
 import { CONFIG } from "./config.js"
 
-export async function fetchData(type, material, q) {
-    // const type = 'pot'
-    // const material = 'porcelain'
+export async function fetchData(type, material, q, involvedMaker, objectNumber) {
     let url =  `${CONFIG.BASE_URL}?key=${CONFIG.API}&ps=100`
+
     if (type) {
         url += `&type=${type}`
     }
@@ -15,12 +14,14 @@ export async function fetchData(type, material, q) {
     if (q) {
         url += `&q=${q}`
     }
+    if (involvedMaker) {
+        url += `&involvedMaker=${involvedMaker}`
+    }
+    if (objectNumber) {
+        url = `${CONFIG.BASE_URL}/${objectNumber}?key=${CONFIG.API}&ps=100`
+    }
 
-    const args = {'type': type, 'material': material, 'q': q};
-
-    // const urlAPI = `${CONFIG.BASE_URL}?key=${CONFIG.API}&ps=100`
-    // const urlAPIType = `${CONFIG.BASE_URL}?key=${CONFIG.API}&ps=100` + `&type=${type}`
-    // const urlAPIMaterial = `${CONFIG.BASE_URL}?key=${CONFIG.API}&ps=100` + `&material=${material}`
+    const args = {'type': type, 'material': material, 'q': q, 'involvedMaker': involvedMaker, 'objectNumber': objectNumber};
 
     const response = await fetch(url);
 
@@ -33,26 +34,4 @@ export async function fetchData(type, material, q) {
     } else {
         throw new Error('HTTP ERROR!!!')
     }
-
-
-    // De data wordt opgehaald
-    // .then(response => response.json())
-    // .then(result => {
-    //     console.log('got something');
-    //     console.log(result);
-    //     return result;
-    // }) // Data wordt omgezet naar JSON
-    // .catch(error => {
-    //     console.log(error);
-    // })
-    // return fetch(urlAPIType)
-    // .then(response => response.json())
-    // .catch(error => {
-    //     console.log(error);
-    // })
-    // return fetch(urlAPIMaterial)
-    // .then(response => response.json())
-    // .catch(error => {
-    //     console.log(error);
-    // })
 }
