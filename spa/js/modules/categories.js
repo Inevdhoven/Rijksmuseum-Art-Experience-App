18014f8b5ed8b1954054ead8386fcf16440e8f05
Inevdhoven <inevdhoven@gmail.com>
2023-02-24 16:16:37 +0100

    Added funiture and made changes

--- spa/js/modules/categories.js
+++ spa/js/modules/categories.js
@@ -1,12 +1,15 @@
-import { fetchData } from "./data.js"
-
 export function artists(data) {
     const artists = data.artObjects.map(artObject => artObject.principalOrFirstMaker)
     const uniqueArtists = [...new Set(artists)] // Source: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates 
     let result = '';
 
     const listItems = uniqueArtists.map((artist) => {
-        return `<li><a href="#artists/${artist.id}">${artist}</a></li>`
+        return `
+        <li>
+            <a href="#artist/${artist}">
+                ${artist}
+            </a>
+        </li>`
     });
     
     listItems.map(item => {
@@ -16,41 +19,70 @@ export function artists(data) {
     return result
 }
 
-export async function listJars(items) {
+let link = '';
+console.log()
+
+export function listJars(items) {
     const jars = items.artObjects.map(artObject => artObject)
-    // const uniqueJars = [...new Set(jars)]
     let result = '';
-    // console.log(jars)
 
     const listJars = jars.map((jar) => {
-        console.log(jar.hasImage !== false)
         if(jar.hasImage !== false){
-            // console.log(jar.webImage.url)
-            return `<li><h2>${jar.title}</h2><img src="${jar.webImage.url}"></li>`
+            return `
+            <li>
+                <a href="#details/${jar.objectNumber}">
+                    <h2>${jar.title}</h2>
+                    <img src="${jar.webImage.url}">
+                </a>
+            </li>`
 
+        } else {
+            return ``
         }
-        // if(jar.hasImage === true) {
-        //     return `<li><img src="${jar.webImage.url}"><h2>${jar.title}</h2></li>`
-        // }
     })
-    // console.log(listJars)
 
     listJars.map(item => {
         result += item  // Zorgt dat de listItems worden samengevoegd tot 1 string
     })
 
-    // // console.log(result)
-
     return result;
 
 }
 
-export async function listFurniture(items) {
+export function listFurniture(items) {
     console.log(items)
     // get items
+
+    const furnitures = items.artObjects.map(artObject => artObject)
+   
+    let result = '';
+    console.log(furnitures)
+
+    const listFurniture = furnitures.map((furniture) => {
+        // console.log(jar.hasImage !== false)
+        // console.log(jar)
+        if(furniture.hasImage !== false){
+            return `
+            <li>
+                <a href="#details/${furniture.objectNumber}">
+                    <h2>${furniture.title}</h2>
+                    <img src="${furniture.webImage.url}">
+                </a>
+            </li>`
+        } else {
+            return ``
+        }
+    })
+    // console.log(listJars)
+
+    listFurniture.map(item => {
+        result += item  // Zorgt dat de listItems worden samengevoegd tot 1 string
+    })
+
+    return result;
     
 }
 
-export async function artist(data) {
+export function artist(data) {
     // Als data gelijk is aan de geklikte artiest dan worden alle kunstwerken van die artiest getoond
 }
