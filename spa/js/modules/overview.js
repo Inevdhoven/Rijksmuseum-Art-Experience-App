58e40c0b2d32d28430d6e58c85ed3912ac8841cd
Inevdhoven <inevdhoven@gmail.com>
2023-02-24 16:17:48 +0100

    Added code for overview artists

---
+++ spa/js/modules/overview.js
@@ -0,0 +1,30 @@
+export function artistOverview(artist) {
+    const artWork = artist.artObjects.map(artObject => artObject)
+
+    let result = ''
+    console.log(artWork)
+
+
+
+    const listArtWork = artWork.map((art) => {
+        console.log(art)
+        if(art.hasImage !== false){
+            console.log('test')
+            return `
+            <li>
+                <a href="#details/${art.objectNumber}">
+                    <h2>${art.title}</h2>
+                    <img src="${art.webImage.url}">
+                </a>
+            </li>`
+        } else {
+            return ``
+        }
+    })
+
+    listArtWork.map(art => {
+        result += art  // Zorgt dat de listItems worden samengevoegd tot 1 string
+    })
+
+    return result
+}
