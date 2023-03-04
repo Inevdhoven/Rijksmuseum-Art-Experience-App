2a664f997c677d805f3ba3f83109ab35b71bbe60
Inevdhoven <inevdhoven@gmail.com>
2023-03-04 09:16:37 +0100

    Added code to see full image

--- spa/js/modules/details.js
+++ spa/js/modules/details.js
@@ -1,6 +1,21 @@
 export function details(item) {
     const artWork = item.artObject;
     
-    return `<h2>${artWork.title}</h2>
-    <img src="${artWork.webImage.url}"></img>`;
+    return `
+            <div>
+                <h1>${artWork.title}</h1>
+                <p>${artWork.description}</p>
+            </div>
+            <img src="${artWork.webImage.url}"/>`;
+}
+
+export function showFullImage(item) {
+    const artWorkImage = item.artObject.webImage.url;
+
+    artWorkImage.addEventListener('click', () => {
+        const fullImage = document.createElement('img');
+        fullImage.src = artWorkImage.src;
+        fullImage.classList.add('full-image');
+        document.body.appendChild(fullImage);
+    })
 }
