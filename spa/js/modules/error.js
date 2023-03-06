2af93cc048e2fbcc00e4416804f24add7d8388b9
Inevdhoven <inevdhoven@gmail.com>
2023-03-06 15:33:02 +0100

    Added error state

---
+++ spa/js/modules/error.js
@@ -0,0 +1,9 @@
+
+export function errorState() {
+    return `
+        <div class="error">
+            <h1>Er is iets mis gegaan!</h1>
+            <p>Probeer het later nog eens</p>
+        </div>
+    `
+}
