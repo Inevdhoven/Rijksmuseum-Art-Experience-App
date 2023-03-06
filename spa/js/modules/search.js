4d559b8082f929d2131d168d7941a8d66bc6e43d
Inevdhoven <inevdhoven@gmail.com>
2023-03-06 15:34:01 +0100

    Added search function to show searched data

--- spa/js/modules/search.js
+++ spa/js/modules/search.js
@@ -1,14 +1,59 @@
+import { CONFIG } from "./config.js"
+import { errorState } from "./error.js"
 
-export function search(data) {
-    // const searchButton = document.querySelector('header nav ul li:nth-of-type(2) a')
+export function search() {
 
-        return `<input type="text" list="searchitems" placeholder="Zoeken...">
-         <datalist id="searchitems">
-            <option value="Rembrandt van Rijn"></option>
-            <option value="Johannes Vermeer"></option>
-            <option value="Jacob Isaacksz van Ruisdael"></option>
-            <option value="Jan Havicksz. Steen"></option>
-            <option value="Vincent van Gogh"></option>
-            <option value="Potten"></option>
-         </datalist>`
+    return `<input type="text" list="searchitems" placeholder="Zoeken...">
+            <datalist id="searchitems">
+                <option value="Rembrandt van Rijn"></option>
+                <option value="Johannes Vermeer"></option>
+                <option value="Jacob Isaacksz van Ruisdael"></option>
+                <option value="Jan Havicksz. Steen"></option>
+                <option value="Vincent van Gogh"></option>
+            </datalist>
+            <button type="submit">Zoeken</button>`
+    
+}
+
+export function getSearchData() {
+    const input = document.querySelector('.search input')
+    const searchValue = input.value;
+    const url = `${CONFIG.BASE_URL}?key=${CONFIG.API}&ps=100&q=${searchValue}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const artworks = data.artObjects;
+
+        artworks.map(artwork => {
+            // console.log(artwork)
+
+            displaySearchData(artwork)
+        })
+        
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+export function displaySearchData(artwork) {
+    const searchResults = document.querySelector('.search-results')
+    const searchResult = document.createElement('li')
+    const searchResultLink = document.createElement('a')
+    const searchResultTitle = document.createElement('h2')
+    const searchResultImage = document.createElement('img')
+
+
+    searchResultLink.href = `/#details/${artwork.objectNumber}`
+    searchResultTitle.textContent = artwork.title
+    searchResultImage.src = artwork.webImage.url
+    searchResultImage.alt = artwork.title
+
+    searchResult.appendChild(searchResultLink)
+    searchResultLink.appendChild(searchResultTitle)
+    searchResultLink.appendChild(searchResultImage)
+
+    searchResults.appendChild(searchResult)
 }
