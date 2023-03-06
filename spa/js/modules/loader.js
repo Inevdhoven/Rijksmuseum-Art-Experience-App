4fee67e420c83c40630a53bfdd9ff2e4564c5651
Inevdhoven <inevdhoven@gmail.com>
2023-03-06 15:33:18 +0100

    Changed loader

--- spa/js/modules/loader.js
+++ spa/js/modules/loader.js
@@ -1,10 +1,14 @@
 // Source Loader: https://dev.to/vaishnavs/displaying-loading-animation-on-fetch-api-calls-1e5m
 export function displayLoader() {
-    const loader = document.querySelector('.loader')
-    loader.classList.add('display')
-    setTimeout(() => {
-        loader.classList.remove("display");
-    }, 5000); // to stop loading after some time
+    // const loader = document.querySelector('.loader')
+    const header = document.querySelector('header')
+    const main = document.querySelector('main')
+    main.classList.add('hide')
+    header.classList.add('hide')
+    // loader.classList.add('display')
+    // setTimeout(() => {
+    //     loader.classList.remove("display");
+    // }, 200); // to stop loading after some time
 }
 
 export function hideLoader() {
